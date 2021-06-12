052a3fced338e6aeb1e25e35f6913dd8104ed1d5
Kate-IQA <81325024+Kate-IQA@users.noreply.github.com>
2021-06-12 23:58:32 +0300

    Add files via upload

---
+++ src/test/java/ru/netology/RadioTest.java
@@ -0,0 +1,38 @@
+package ru.netology;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioTest {
+
+    @org.junit.jupiter.api.Test
+    void getCurrentStation() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setCurrentStation() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getCurrentVolume() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setCurrentVolume() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void nextStation() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void prevStation() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void plusVolume() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void minusVolume() {
+    }
+}
